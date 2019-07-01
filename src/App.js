0a5e65007d17ee3c4640ba2638721770554bc7ec
Reagan Davenport <reagandavenport@Reagans-MacBook-Air.local>
2019-07-01 14:48:55 -0400

    a mostly working metranome

--- src/App.js
+++ src/App.js
@@ -1,23 +1,90 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import Metranome from './Metranome';
-// import registerServiceWorker from './registerServiceWorker';
+// import Metranome from './Metranome';
+import click1 from './click1.wav';
+import click2 from './click2.wav'
 import './index.css';
 import './App.css';
 
-ReactDOM.render(<Metranome/>, document.getElementById('root'));
+// ReactDOM.render(<Metranome/>, document.getElementById('root'));
 // registerServiceWorker();
 
 
 
 class App extends Component {
-  state = {
-    playing: false,
-    count: 0,
-    bpm: 100,
-    beatsPerMeasure: 4
+  constructor(props) {
+    super(props);
+    this.state = {
+      playing: false,
+      count: 0,
+      bpm: 100,
+      beatsPerMeasure: 4
+    };
+
+    this.click1 = new Audio(click1);
+    this.click2 = new Audio(click2);
   };
   
+
+  handleBpmChange = event => {
+    const bpm = event.target.value;
+
+    if (this.state.playing) {
+    // Stop the old timer and start a new one
+      clearInterval(this.timer);
+      this.timer = setInterval(this.playClick, (60 / bpm) * 1000);
+
+    // Set the new BPM, and reset the beat counter
+      this.setState({
+        count: 0,
+        bpm
+      });
+    } else {
+    // Otherwise just update the BPM
+      this.setState({ bpm });
+  }
+};
+
+  startStop = () => {
+    if (this.state.playing) {
+      // Stop the timer
+      clearInterval(this.timer);
+      this.setState({
+        playing: false
+      });
+    } else {
+      // Start a timer with the current BPM
+      this.timer = setInterval(
+        this.playClick,
+        (60 / this.state.bpm) * 1000
+      );
+      this.setState(
+        {
+          count: 0,
+          playing: true
+          // Play a click "immediately" (after setState finishes)
+        },
+        this.playClick
+      );
+    };
+  };
+
+  playClick = () => {
+    const { count, beatsPerMeasure } = this.state;
+  
+    // The first beat will have a different sound than the others
+    if (count % beatsPerMeasure === 0) {
+      this.click2.play();
+    } else {
+      this.click1.play();
+    };
+  
+    // Keep track of which beat we're on
+    this.setState(state => ({
+      count: (state.count + 1) % state.beatsPerMeasure
+    }));
+  };
+
   render(){
     let bpm = 100;
     let playing = false;
@@ -26,12 +93,16 @@ class App extends Component {
         <div className="App">
         <div className="bpm-slider">
         <div>{bpm} BPM</div>
-        <input type="range" min="60" max="240" value={bpm} />
+        <input type="range" 
+          min="60" 
+          max="240" 
+          value={bpm}
+          onChange={this.handleBpmChange}/>
         </div>
-        <button>{playing ? 'Stop' : 'Start'}</button>
+        <button onClick={this.startStop}>{playing ? 'Stop' : 'Start'}</button>
         </div>
     );
+  };
 };
-}
 
 export default App;
